12542997858c50f2c3584752221df2bfdd25b15a
Danillo Fratta <danillo.fratta@gmail.com>
2025-02-07 22:49:38 -0300

    Refactoring, removing files

--- src/backend/Ambev.Sale.Core.Domain.Entities/Common/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ambev.Sale.Core.Domain.Entities.Common;
-
-public class BaseEntity 
-{
-    public Guid Id { get; set; }
-
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
